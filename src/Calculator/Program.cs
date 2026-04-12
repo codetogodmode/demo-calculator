3a8c5765a6204bd7bd1dee48e793bc6db6d13580
Martin Skořupa <martin.skorupa.personal@outlook.com>
2026-04-12 22:19:31 +0200

    Added calculation output instead of hello world

--- src/Calculator/Program.cs
+++ src/Calculator/Program.cs
@@ -4,7 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Na Session 1 se přepíše na "2 + 2 = 4"
-        Console.WriteLine("Hello from Calculator!");
+        Console.WriteLine("2+2=4");
     }
 }
